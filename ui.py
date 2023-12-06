fb887392a1626e1e099a684c14d4281a5373bf44
ksmishmash <kallask@oregonstate.edu>
2023-12-06 10:12:42 -0800

    User Interface - outline for user interface as ui.py

---
+++ ui.py
@@ -0,0 +1,27 @@
+# Author: Karina Kallas
+# Date Last Modified: Dec. 6, 2023
+#
+# Project Name: Master Mind
+# Project Description:  A game which can be played by a user "against" the computer.
+#                       This is a game where a player tries to guess the number combinations. At the end of each attempt
+#                       to guess the 4 number combinations, the computer will provide feedback whether the player had
+#                       guess a number correctly, or/and a number and digit correctly. A player must guess the right
+#                       number combinations within 10 attempts to win the game.
+#
+# File Description: ui.py calls program and organizes actions.
+
+
+def main():
+    # print("welcome")
+
+    # instructions
+
+    # while player wants to continue:
+    #   play game?
+
+    # exit
+    return
+
+
+if __name__ == '__main__':
+    main()

6163e61d422c65ac83907e279a24c11dba3ff390
ksmishmash <kallask@oregonstate.edu>
2023-12-11 13:19:45 -0800

    Add tests to test_code_maker.py for check_player_guess(), int_list_to_string(), and check_end_of_game().

--- tests/test_code_maker.py
+++ tests/test_code_maker.py
@@ -42,6 +42,31 @@ class TestCodeMaker(unittest.TestCase):
         self.code_maker.create_random_code()
         self.assertEqual(len(self.code_maker.answer_code), self.code_maker.code_entries,
                          "The answer_code is the same length as the code_entries.")
+        self.assertEqual(type(self.code_maker.answer_code), list,
+                         "The function create_random_code did not return a list.")
+
+    # Do zero turns make game return end=True? If all guesses correct, is win=True?
+    def test_check_end_of_game(self):
+        self.code_maker.turns = 0
+        result = self.code_maker.check_end_of_game(0)       # result = (end, win)
+        self.assertEqual(result, (True, False), "Zero turns does not trigger game to exit.")
+        result = self.code_maker.check_end_of_game(4)
+        self.assertEqual(result, (True, True),
+                         "Correct code entries matching correct match_val_and_place does not trigger exit.")
+
+    # Does check answer
+    def test_check_player_guess(self):
+        self.code_maker.answer_code = [1, 2, 3, 4]
+        inputs = [[1, 2, 3, 4], [4, 3, 2, 1], [0, 5, 6, 7], [1, 4, 1, 1]]
+        answers = [(4, 0), (0, 4), (0, 0), (1, 1)]
+        for idx in range(4):
+            result = self.code_maker.check_player_guess(inputs[idx])   # (match_val_and_place: int, match_val: int)
+            self.assertEqual(result, answers[idx], f"Matches incorrect: guess {inputs[idx]} returns {answers[idx]}.")
+
+    # Check that formats as string.
+    def test_int_list_to_string(self):
+        result = self.code_maker.int_list_to_string([1, 2, 3, 4])
+        self.assertEqual(type(result), str, "Does not return string.")
 
 
 if __name__ == "__main__":

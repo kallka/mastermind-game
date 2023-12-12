cfba275e910793f9df15d473cd1beeba2eda3e46
ksmishmash <kallask@oregonstate.edu>
2023-12-12 12:45:50 -0800

    Move docstring above function

--- ui.py
+++ ui.py
@@ -31,14 +31,14 @@ from messages_constants import WELCOME, PLAY_GAME, INSTRUCTIONS, GOODBYE, YES_RE
 #      - play_mastermind()                                                                                             #
 #                                                                                                                      #
 # #################################################################################################################### #
+'''
+Sets up loop that asks player if they would like to play the game(s).
+Player needs to return a valid yes/no response from get_user_input_yes_no in order for loop to continue.
+If break_loop return from get_user_input_yes_no function is equal to the break_loop_max (number of tries
+given to a playerr to attempt correct input), the program returns.
+Returns when player declines to play another game or break_loop_max reached.
+'''
 def ask_to_play() -> None:
-    '''
-    Sets up loop that asks player if they would like to play the game(s).
-    Player needs to return a valid yes/no response from get_user_input_yes_no in order for loop to continue.
-    If break_loop return from get_user_input_yes_no function is equal to the break_loop_max (number of tries
-    given to a playerr to attempt correct input), the program returns.
-    Returns when player declines to play another game or break_loop_max reached.
-    '''
     play = True
 
     while play:
@@ -57,14 +57,14 @@ def ask_to_play() -> None:
             play = False
 
 
+'''
+Gets a yes or no user response. Loops through request to get input until a user either
+returns a valid yes/no (ex. 'YES, yES, y, No, N, no') or until the break_loop_max is reached.
+If the user can't return a valid response in this many times, the program returns.
+:param break_loop_max: max number of times to ask for valid response
+:return: user_response (string), break_loop (int) so that another function knows valid response or if max was reached
+'''
 def get_user_input_yes_no(break_loop_max: int) -> tuple:
-    '''
-    Gets a yes or no user response. Loops through request to get input until a user either
-    returns a valid yes/no (ex. 'YES, yES, y, No, N, no') or until the break_loop_max is reached.
-    If the user can't return a valid response in this many times, the program returns.
-    :param break_loop_max: max number of times to ask for valid response
-    :return: user_response (string), break_loop (int) so that another function knows valid response or if max was reached
-    '''
     user_response = ''
     break_loop = 0
 
@@ -82,15 +82,15 @@ def get_user_input_yes_no(break_loop_max: int) -> tuple:
     return user_response, break_loop
 
 
+'''
+Alerts the user about remaining turns and gets user's code guess.
+Informs user as to current parameters for guess.
+Validation is performed in the play_mastermind() function after guess is returned.
+Additionally, user can input 'h' to get a hint.
+:param game: The instance of game created by the player (CodeBreaker).
+:return: guess (string input from player)
+'''
 def get_user_guess(game: object) -> str:
-    '''
-    Alerts the user about remaining turns and gets user's code guess.
-    Informs user as to current parameters for guess.
-    Validation is performed in the play_mastermind() function after guess is returned.
-    Additionally, user can input 'h' to get a hint.
-    :param game: The instance of game created by the player (CodeBreaker).
-    :return: guess (string input from player)
-    '''
     print(f"{REMAINING_TURNS.format(turns=game.remaining_turns())}\t{GET_HINT}\t{GET_PRINT}")
 
     entries, min_num, max_num = game.current_game.get_code_entries(), \
@@ -117,12 +117,12 @@ def get_hints(game: object) -> str:
     return guess
 
 
+'''
+Prints instructions and creates an instance of the game.
+Performs a while loop to make guesses while turns remain.
+:return: None
+'''
 def play_mastermind() -> None:
-    '''
-    Prints instructions and creates an instance of the game.
-    Performs a while loop to make guesses while turns remain.
-    :return: None
-    '''
     # TODO: This where the game could include more than 1 user. Can include set for CodeBreaker's answer code.
     # TODO: A GameManager class might help control play as users increase.
     game = CodeBreaker()
@@ -148,11 +148,7 @@ def play_mastermind() -> None:
 #                                                                                                                      #
 # #################################################################################################################### #
 def main():
-    '''
-    Welcomes user(s), asks if users want to play, and says goodbye.
-    :return: 0
-    '''
-
+    # Welcomes user(s), asks if users want to play, and says goodbye. Returns 0
     print(WELCOME)
     ask_to_play()
     print(f"\n{GOODBYE}")
